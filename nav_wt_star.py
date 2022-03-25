048287233b69f9ae9855c24cb955c7990b0d3b45
gnath12 <87157783+gnath12@users.noreply.github.com>
2022-03-25 10:24:48 +0900

    Update nav_wt_star.py

--- nav_wt_star.py
+++ nav_wt_star.py
@@ -21,13 +21,15 @@ for week_list in new_url[1:-1]:
     r = requests.get(week_list)
     soup = bs(r.text, "html.parser")
 
-    web_list = soup.select("ul[class=img_list]")
+    web_list = soup.select("ul[class=img_list] > li")
     for web in web_list:
-        title = soup.select("ul[class=img_list] > li > div[class=thumb] > a")[0].attrs["title"]
-        st_scr = soup.select("div[class=rating_type] > strong")[0].text
+        title = soup.select("div[class=thumb] > a")[0].attrs["title"]
+    #     st_scr = soup.select("div[class=rating_type] > strong")[0].text
+    
         print(title)
 
 
-        with open("uak.txt", "w", encoding="UTF-8") as f:
-            for title, st_scr in nav_web.items():
-                f.write(f"{title}{st_scr}\n")
+
+        # with open("uak.txt", "w", encoding="UTF-8") as f:
+        #     for title, st_scr in nav_web.items():
+        #         f.write(f"{title}{st_scr}\n")
